5f3b0f9a25ae6e6ff4533bf571a17f02af1744a3
IgorMT100 <118048429+IgorMT100@users.noreply.github.com>
2022-11-13 17:33:14 +0300

    Update 8.jl

--- n8/8.jl
+++ n8/8.jl
@@ -18,27 +18,4 @@ function spiral(robot)
         end
     end
 end
-#=
-function spiral(robot)
-    side = West
-    num_steps = 1
-    check_steps = 0
-    a = true
-    while a
-        side = right(side)
-        for i in 1:num_steps
-            move!(robot,side)
-            if ismarker(robot)
-                a = false
-                break
-            end
-        end
-        check_steps +=1
-        if check_steps%2==0
-            num_steps +=1
-        end
-    end
-
-end
-=#
 right(side::HorizonSide)::HorizonSide = HorizonSide(mod(Int(side) - 1 , 4)) 
